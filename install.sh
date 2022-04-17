c89dc71cf68b0cfff46d2402c0910060953611e3
hristotanev <hristotanev7@gmail.com>
2022-04-17 23:27:35 +0100

    update scripts paths

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for script_path in $(pwd)/tools/*.sh; do
+for script_path in $(pwd)/scripts/*.sh; do
   script_name=$(basename $script_path | awk -F'.' '{ print $1 }')
   sudo ln -s $script_path /usr/bin/$script_name
 done
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for script_path in $(pwd)/tools/*.sh; do
+for script_path in $(pwd)/scripts/*.sh; do
   script_name=$(basename $script_path | awk -F'.' '{ print $1 }')
   sudo rm /usr/bin/$script_name
 done
