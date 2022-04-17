f8f95c246d8d539cdea26ae0bb7bace3e4fa17fc
hristotanev <hristotanev7@gmail.com>
2022-04-17 22:48:51 +0100

    add vhash script

---
+++ tools/vhash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HASH=$1
+FILE=$2
+
+function usage {
+  echo "Usage: vhash [HASH] [FILE]"
+  echo "Verify SHA256 hash matches for downloaded FILE."
+  echo
+  echo -e "  -h\t display this help and exit"
+  exit
+}
+
+while getopts "h" flag; do
+  case $flag in
+    h) usage; exit;;
+    ?) echo "Run '-h' for more info."; exit;;
+  esac
+done
+
+if [[ $HASH == "" || $FILE == "" ]]; then
+  echo "Run '-h' for more information on required command options."
+  exit
+fi
+
+sha256sum $FILE && sha256sum -c $HASH
