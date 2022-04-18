25cdc9d2c22a9b2b27214df0e462553d4071aa07
hristotanev <hristotanev7@gmail.com>
2022-04-18 11:09:19 +0100

    install only missing tools + rename

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 for script_path in $(pwd)/scripts/*.sh; do
   script_name=$(basename $script_path | awk -F'.' '{ print $1 }')
-  sudo ln -s $script_path /usr/bin/$script_name
+  if [[ $(ls /usr/bin/ | grep -o -w $script_name) == "" ]]; then
+    sudo ln -s $script_path /usr/bin/$script_name
+  else
+    echo "'$script_name' already installed. Skipping..."
+  fi
 done
