0735a944fa3feb6f68d99f06237f24207792d7b2
hristotanev <hristotanev7@gmail.com>
2022-04-20 15:26:52 +0100

    devsak can't be uninstalled multiple times

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ ! -e $HOME/.config/devsak ]]; then
-  echo "'devsak' has already been uninstalled. skipping."
-  exit
-fi
-
 for script_path in $HOME/.config/devsak/scripts/*.sh; do
   script_name=$(basename $script_path | awk -F'.' '{ print $1 }')
   if [[ $(ls /usr/local/bin | grep -o -w "$script_name") != "" ]]; then
