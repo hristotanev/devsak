f8107a73d804a5efd0d0e203d4186bf742ef1814
hristotanev <hristotanev7@gmail.com>
2022-04-20 22:06:17 +0100

    rearrange dirs

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ if [[ -e $HOME/.config/devsak ]]; then
 fi
 
 git clone https://github.com/hristotanev/devsak.git $HOME/.config/devsak
-sudo ln -s $HOME/.config/devsak/devsak.sh /usr/local/bin/devsak
-sudo ln -s $HOME/.config/devsak/uninstall.sh /usr/local/bin/uninstall_devsak
+sudo ln -s $HOME/.config/devsak/scripts/devsak.sh /usr/local/bin/devsak
+sudo ln -s $HOME/.config/devsak/scripts/uninstall.sh /usr/local/bin/uninstall_devsak
 
 echo "'devsak' installed successfully. don't forget to restart your terminal."
