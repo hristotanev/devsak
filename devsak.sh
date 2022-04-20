0e61cbc4d46d063466b56f11d48278440c7807e6
hristotanev <hristotanev7@gmail.com>
2022-04-20 21:25:25 +0100

    add option to install specific scripts

--- devsak.sh
+++ devsak.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
 function usage {
-  echo "Usage: devsak [OPTIONS]"
-  echo "Command-line orchestrator for installing different scripts on devsak."
+  echo "Usage: devsak [OPTIONS] <SCRIPT1,SCRIPT2,...>"
+  echo "Command-line orchestrator for installing different scripts."
   echo
   echo "Some of the available options include:"
   echo
-  echo -e "  -a\t install all available scripts"
-  echo -e "  -h\t display this help and exit"
+  echo -e "  -a\t\t\t install all available scripts"
+  echo -e "  -s SCRIPT1,SCRIPT2,... install scripts specified by their name, comma separated (list all scripts for more info)"
+  echo -e "  -h\t\t\t display this help and exit"
+  exit
+}
+
+function install_script_if_missing {
+  script_name=$1
+  if [[ $(ls /usr/local/bin/ | grep -o -w $script_name) == "" ]]; then
+    echo "Installing '$script_name' script..."
+    sudo ln -s $HOME/.config/devsak/scripts/$script_name.sh /usr/local/bin/$script_name
+  else
+    echo "'$script_name' already installed. skipping."
+  fi
   exit
 }
 
 function install_all {
   for script_path in $HOME/.config/devsak/scripts/*.sh; do
     script_name=$(basename $script_path | awk -F'.' '{ print $1 }')
-    if [[ $(ls /usr/local/bin/ | grep -o -w $script_name) == "" ]]; then
-      echo "Installing '$script_name' script..."
-      sudo ln -s $script_path /usr/local/bin/$script_name
-    else
-      echo "'$script_name' already installed. skipping."
-    fi
+    install_script_if_missing $script_name
   done
 
   exit
 }
 
-while getopts "a,h" flag; do
+function install_scripts {
+  scripts=$(echo "$OPTARG" | tr ',' ' ')
+  for script_name in $scripts; do
+    install_script_if_missing $script_name
+  done
+
+  exit
+}
+
+while getopts "a,s:h" flag; do
   case $flag in
     a) install_all; exit;;
+    s) install_scripts; exit;;
     h) usage; exit;;
     ?) echo "Run '-h' for more info."; exit;;
   esac
